55a20b32868f195b1a96d3552641b8458e131d9a
Snevver <svenhoeksema@hotmail.com>
2025-05-07 09:40:15 +0200

    Update db.php

--- Database/db.php
+++ Database/db.php
@@ -10,7 +10,7 @@ $user = $env['USER'];
 $password = $env['PASSWORD'];
 
 if (!function_exists('pg_connect')) {
-    die("PostgreSQL extension is not installed. Please install the php-pgsql extension.\n");
+    die("PostgreSQL extension is not installed. Please install the php-pgsql extension by uncommenting / adding 'extension=php-pgsql' in your php.ini file.\n");
 }
 
 $connection_string = "host=$host port=$port dbname=$dbname user=$user password=$password sslmode=require";
