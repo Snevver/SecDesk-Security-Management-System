9682b527484429afa78fcc266421041186f125f0
donbithub <doncandayan@msn.com>
2025-05-28 11:33:20 +0200

    targets first ver
    added css
    changed target.js for dom
    added html to targets

--- public/js/targets.js
+++ public/js/targets.js
@@ -1,275 +1,277 @@
 // Get the URL parameters
 const urlParams = new URLSearchParams(window.location.search);
-const test_id = urlParams.get('test_id');
-const target_id = urlParams.get('target_id');
+const test_id = urlParams.get("test_id");
+const target_id = urlParams.get("target_id");
 
 function fetchTestTargets(test_id) {
-    console.log(`Fetching targets for test ID: ${test_id}`);
-    fetch(`/api/targets?test_id=${test_id}`)
-        .then((response) => {
-            if (!response.ok) {
-                console.log(response);
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then((data) => {
-            const targetListElement =
-                document.getElementsByClassName('target-list')[0];
-            if (!data || !data.targets || data.targets.length === 0) {
-                targetListElement.innerHTML = '<p>No targets found.</p>';
-                return;
-            }
-            let targetList = '';
-            for (let target of data.targets) {
-                targetList += `
-                    <div id="target-${target.id}">
-                        <h2>
-                            ${target.target_name}
-                        </h2>
-                        <div>
-                            <p>${target.target_description}</p>
-                        </div>
-                    </div>`;
-            }
+  console.log(`Fetching targets for test ID: ${test_id}`);
+  fetch(`/api/targets?test_id=${test_id}`)
+    .then((response) => {
+      if (!response.ok) {
+        console.log(response);
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const targetListElement =
+        document.getElementsByClassName("target-list")[0];
+      if (!data || !data.targets || data.targets.length === 0) {
+        targetListElement.innerHTML = "<p>No targets found.</p>";
+        return;
+      }
+      let targetList = "";
+      for (let target of data.targets) {
+        targetList += `
+                    <button class="target-button p-0 d-flex" id="target-${target.id}"
+                            data-bs-toggle="tooltip"
+                            data-bs-placement="bottom"
+                            data-bs-title="${target.target_description}"
+                            data-bs-custom-class="custom-tooltip">
+                        <p class="ms-1">
+                            T${target.id}: ${target.target_name}
+                        </p>
+                    </button>`;
+      }
 
-            targetListElement.innerHTML = targetList;
+      targetListElement.innerHTML = targetList;
 
-            // Add event listeners after elements are added to DOM
-            const targetElements =
-                targetListElement.querySelectorAll("div[id^='target-']");
-            targetElements.forEach((element) => {
-                element.addEventListener('click', function (e) {
-                    e.preventDefault();
-                    const targetId = this.id.replace('target-', '');
-                    console.log(`Clicking on target ${targetId}`);
-                    fetchVulnerabilities(targetId);
-                });
-            });
-        })
-        .catch((error) => {
-            console.error(
-                'There was a problem with the fetch operation:',
-                error,
-            );
+      // Enabling Bootstrap tooltips with delay, only working on hover not focus
+      const newButtons = document.querySelectorAll(
+        '[data-bs-toggle="tooltip"]'
+      );
+      newButtons.forEach(
+        (el) =>
+          new bootstrap.Tooltip(el, {
+            delay: { show: 500, hide: 200 },
+            trigger: "hover",
+          })
+      );
+
+      // Add event listeners after elements are added to DOM
+      const targetElements = targetListElement.querySelectorAll(
+        "button[id^='target-']"
+      );
+      targetElements.forEach((element) => {
+        element.addEventListener("click", function (e) {
+          e.preventDefault();
+
+          // Remove 'focused' class from all first, then add 'focused to the clicked element
+          targetElements.forEach((el) => el.classList.remove("active"));
+          this.classList.add("active");
+
+          const targetId = this.id.replace("target-", "");
+          console.log(`Clicking on target ${targetId}`);
+          fetchVulnerabilities(targetId);
         });
+      });
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
 }
 
 function fetchVulnerabilities(target_id) {
-    console.log(`Fetching vulnerabilities for target ID: ${target_id}`);
-    fetch(`/api/vulnerabilities?target_id=${target_id}`)
-        .then((response) => {
-            if (!response.ok) {
-                console.log(response);
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then((data) => {
-            console.log('Vulnerabilities:', data.vulnerabilities);
-            const vulnerabilityListElement =
-                document.getElementsByClassName('vulnerability-list')[0];
-            if (
-                !data ||
-                !data.vulnerabilities ||
-                data.vulnerabilities.length === 0
-            ) {
-                vulnerabilityListElement.innerHTML =
-                    '<p>No vulnerabilities found.</p>';
-                return;
-            }
+  console.log(`Fetching vulnerabilities for target ID: ${target_id}`);
+  fetch(`/api/vulnerabilities?target_id=${target_id}`)
+    .then((response) => {
+      if (!response.ok) {
+        console.log(response);
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("Vulnerabilities:", data.vulnerabilities);
+      const vulnerabilityListElement =
+        document.getElementsByClassName("vulnerability-list")[0];
+      if (!data || !data.vulnerabilities || data.vulnerabilities.length === 0) {
+        vulnerabilityListElement.innerHTML = "<p>No vulnerabilities found.</p>";
+        return;
+      }
 
-            let vulnerabilityList = '';
-            for (let vulnerability of data.vulnerabilities) {
-                vulnerabilityList += `
-                    <div id="vulnerability-${vulnerability.id}">
-                        <h3>
-                            ${vulnerability.affected_entity}
-                        </h3>
-                        <div>
-                            <p><strong>Classification:</strong>
-                                ${vulnerability.classification}
-                            </p>
-                            <p><strong>CVSS Score:</strong>
-                                ${vulnerability.cvss_score}
-                            </p>
-                            <p><strong>Risk:</strong>
-                                ${vulnerability.likelihood}
-                            </p>
-                            <p>${vulnerability.vulnerabilities_description}</p>
-                        </div>
-                    </div>`;
-            }
+      let vulnerabilityList = "";
+      for (let vulnerability of data.vulnerabilities) {
+        vulnerabilityList += `
+                    <button id="vulnerability-${vulnerability.id}" class="d-flex target-button p-0">
+                        <p class="ms-1">
+                            V${vulnerability.id}: ${vulnerability.affected_entity}
+                        </p>
+                    </button>`;
+      }
 
-            vulnerabilityListElement.innerHTML = vulnerabilityList;
+      vulnerabilityListElement.innerHTML = vulnerabilityList;
 
-            // Add event listeners for vulnerability clicks after elements are added to DOM
-            const vulnerabilityElements =
-                vulnerabilityListElement.querySelectorAll(
-                    "div[id^='vulnerability-']",
-                );
-            vulnerabilityElements.forEach((element) => {
-                element.addEventListener('click', function (e) {
-                    e.preventDefault();
-                    const vulnerabilityId = this.id.replace(
-                        'vulnerability-',
-                        '',
-                    );
-                    console.log(`Clicking on vulnerability ${vulnerabilityId}`);
+      // Add event listeners for vulnerability clicks after elements are added to DOM
+      const vulnerabilityElements = vulnerabilityListElement.querySelectorAll(
+        "button[id^='vulnerability-']"
+      );
+      vulnerabilityElements.forEach((element) => {
+        element.addEventListener("click", function (e) {
+          e.preventDefault();
 
-                    // Find the vulnerability data from the response
-                    const vulnerability = data.vulnerabilities.find(
-                        (vulnerability) => vulnerability.id == vulnerabilityId,
-                    );
-                    if (vulnerability) {
-                        showVulnerabilityDetails(vulnerability);
-                    }
-                });
-            });
-        })
-        .catch((error) => {
-            console.error(
-                'There was a problem with the fetch operation:',
-                error,
-            );
+          // Remove 'focused' class from all first, then add 'focused to the clicked element
+          vulnerabilityElements.forEach((el) => el.classList.remove("active"));
+          this.classList.add("active");
+
+          const vulnerabilityId = this.id.replace("vulnerability-", "");
+          console.log(`Clicking on vulnerability ${vulnerabilityId}`);
+
+          // Find the vulnerability data from the response
+          const vulnerability = data.vulnerabilities.find(
+            (vulnerability) => vulnerability.id == vulnerabilityId
+          );
+          if (vulnerability) {
+            showVulnerabilityDetails(vulnerability);
+          }
         });
+      });
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
 }
 
 function showVulnerabilityDetails(vulnerability) {
-    console.log('Showing details for vulnerability:', vulnerability);
+  console.log("Showing details for vulnerability:", vulnerability);
 
-    const detailsElement = document.getElementsByClassName('vulnerability-details')[0];
-    const detailsHtml = `
-        <div>
-            <h2>Vulnerability Details</h2>
-            
-            <div>
-                <div>
-                    <h3>Basic Information</h3>
-                    <p><strong>Affected Entity:</strong>
-                        ${vulnerability.affected_entity || 'N/A'}
-                    </p>
-                    <p><strong>Identifier:</strong>
-                        ${vulnerability.identifier || 'N/A'}
-                    </p>
-                    <p><strong>Classification:</strong>
-                        ${vulnerability.classification || 'N/A'}
-                    </p>
-                    <p><strong>Location:</strong>
-                        ${vulnerability.location || 'N/A'}
-                    </p>
-                    <p><strong>Affected Component:</strong>
-                        ${vulnerability.affected_component || 'N/A'}
-                    </p>
-                </div>
-                
-                <div>
-                    <h3>Risk Assessment</h3>
-                    <p><strong>CVSS Score:</strong>
-                        ${vulnerability.cvss_score || 'N/A'}
-                    </p>
-                    <p><strong>CVSS v3 Code:</strong>
-                        ${vulnerability.cvssv3_code || 'N/A'}
-                    </p>
-                    <p><strong>Likelihood:</strong>
-                        ${vulnerability.likelihood || 'N/A'}
-                    </p>
-                    <p><strong>Remediation Difficulty:</strong>
-                        ${vulnerability.remediation_difficulty || 'N/A'}
-                    </p>
-                    <p><strong>Status:</strong>
-                        ${vulnerability.solved ? 'Solved' : 'Open'}
-                    </p>
-                </div>
-            </div>
-            
-            <div>
+  const detailsElement = document.getElementsByClassName(
+    "vulnerability-details"
+  )[0];
+  const detailsHeaderElement = document.getElementById("vulnerabilityDetails");
+
+  const detailsHeaderHtml = `
+        <h3 class="text-center w-100">V${vulnerability.id}: ${vulnerability.affected_entity}</h3>
+  `;
+
+  const detailsHtml = `
+<div class="divTable">
+  <div class="divTableBody">
+
+    <div class="divTableRow">
+      <div class="divTableCell"><strong>Identifier:</strong></div>
+      <div class="divTableCell">${vulnerability.identifier || "N/A"}</div>
+      <div class="divTableCell"><strong>CVSS Score:</strong></div>
+      <div class="divTableCell">${vulnerability.cvss_score || "N/A"}</div>
+      <div class="divTableCell"><strong>Risk Statement:</strong></div>
+      <div class="divTableCell">${vulnerability.risk_statement || "N/A"}</div>
+    </div>
+
+    <div class="divTableRow">
+      <div class="divTableCell"><strong>Classification:</strong></div>
+      <div class="divTableCell">${vulnerability.classification || "N/A"}</div>
+      <div class="divTableCell"><strong>CVSS v3 Code:</strong></div>
+      <div class="divTableCell">${vulnerability.cvssv3_code || "N/A"}</div>
+      <div class="divTableCell"><strong>Residual Risk:</strong></div>
+      <div class="divTableCell">${vulnerability.residual_risk || "N/A"}</div>
+    </div>
+
+    <div class="divTableRow">
+      <div class="divTableCell"><strong>Location:</strong></div>
+      <div class="divTableCell">${vulnerability.location || "N/A"}</div>
+      <div class="divTableCell"><strong>Likelihood:</strong></div>
+      <div class="divTableCell">${vulnerability.likelihood || "N/A"}</div>
+      <div class="divTableCell"><strong>Identified Controls:</strong></div>
+      <div class="divTableCell">${
+        vulnerability.identified_controls || "N/A"
+      }</div>
+    </div>
+
+    <div class="divTableRow">
+      <div class="divTableCell"><strong>Affected Component:</strong></div>
+      <div class="divTableCell">${
+        vulnerability.affected_component || "N/A"
+      }</div>
+      <div class="divTableCell"><strong>Remediation Difficulty:</strong></div>
+      <div class="divTableCell">${
+        vulnerability.remediation_difficulty || "N/A"
+      }</div>
+      <div class="divTableCell"><strong>Status:</strong></div>
+      <div class="divTableCell">${
+        vulnerability.solved ? "Solved" : "Open"
+      }</div>
+    </div>
+
+  </div>
+</div>
+
+
+
+            <div class="ms-2 mt-2">
                 <h3>Description</h3>
-                <p>${vulnerability.vulnerabilities_description || 'N/A'}</p>
+                <p>${vulnerability.vulnerabilities_description || "N/A"}</p>
             </div>
-            
-            <div>
-                <h3>Risk Statement</h3>
-                <p>${vulnerability.risk_statement || 'N/A'}</p>
-            </div>
+
               ${
-                  vulnerability.reproduction_steps
-                      ? `<div>
+                vulnerability.reproduction_steps
+                  ? `<div>
                     <h3>Reproduction Steps</h3>
                     <pre>${vulnerability.reproduction_steps}</pre>
                 </div>`
-                      : ''
+                  : ""
               }
-            
+
             ${
-                vulnerability.impact
-                    ? `<div>
+              vulnerability.impact
+                ? `<div>
                     <h3>Impact</h3>
                     <p>${vulnerability.impact}</p>
                 </div>`
-                    : ''
+                : ""
             }
-            
-            <div>
-                <h3>Identified Controls</h3>
-                <p>${vulnerability.identified_controls || 'N/A'}</p>
-            </div>
-            
-            <div>
-                <h3>Residual Risk</h3>
-                <p>${vulnerability.residual_risk || 'N/A'}</p>
-            </div>
-            
-            <div>
+
+            <div class="ms-2">
                 <h3>Recommendations</h3>
-                <p>${vulnerability.recommendations || 'N/A'}</p>
+                <p>${vulnerability.recommendations || "N/A"}</p>
             </div>
               ${
-                  vulnerability.recommended_reading
-                      ? `<div>
+                vulnerability.recommended_reading
+                  ? `<div>
                     <h3>Recommended Reading</h3>
                     <p>${vulnerability.recommended_reading}</p>
                 </div>`
-                      : ''
+                  : ""
               }
-            
+
             ${
-                vulnerability.response
-                    ? `<div>
+              vulnerability.response
+                ? `<div>
                     <h3>Response</h3>
                     <p>${vulnerability.response}</p>
                 </div>`
-                    : ''
+                : ""
             }
-            
-            <div>
+
+            <div class="ms-2">
                 <button onclick="hideVulnerabilityDetails()">Close Details</button>
             </div>
         </div>
     `;
 
-    detailsElement.innerHTML = detailsHtml;
-    detailsElement.scrollIntoView({ behavior: 'smooth' });
+  detailsElement.innerHTML = detailsHtml;
+  detailsElement.scrollIntoView({ behavior: "smooth" });
+
+  detailsHeaderElement.innerHTML = detailsHeaderHtml;
 }
 
 function hideVulnerabilityDetails() {
-    const detailsElement = document.getElementsByClassName(
-        'vulnerability-details',
-    )[0];
-    detailsElement.innerHTML = '';
+  const detailsElement = document.getElementsByClassName(
+    "vulnerability-details"
+  )[0];
+  detailsElement.innerHTML = "";
 }
 
 // Fetch the test targets when the script loads
 fetchTestTargets(test_id);
 
 // Fetch the vulnerabilities of a target when a target is clicked
-addEventListener('DOMContentLoaded', function (clickEvent) {
-    const targetLinks = document.querySelectorAll("a[id^='target-']");
-    targetLinks.forEach((link) => {
-        link.addEventListener('click', function (e) {
-            e.preventDefault();
-            const targetId = this.id.replace('target-', '');
-            fetchVulnerabilities(targetId);
-        });
+addEventListener("DOMContentLoaded", function (clickEvent) {
+  const targetLinks = document.querySelectorAll("a[id^='target-']");
+  targetLinks.forEach((link) => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      const targetId = this.id.replace("target-", "");
+      fetchVulnerabilities(targetId);
     });
+  });
 });
