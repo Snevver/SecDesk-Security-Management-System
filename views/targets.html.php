87ae1cca182448973c0b818702aba1430c25ad6b
donbithub <doncandayan@msn.com>
2025-06-11 10:23:32 +0200

    targets html/js/css
    fixed accordion css
    changed js dom
    added admin/employee css for linking backend

--- views/targets.html.php
+++ views/targets.html.php
@@ -8,7 +8,6 @@
     <!-- CSS -->
     <link rel="stylesheet" href="/css/main.css">
     <link rel="stylesheet" href="/css/index.css">
-    <!-- <link rel="stylesheet" href="./public/css/targets.css"> -->
     <link rel="stylesheet" href="/css/bootstrap.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
 
@@ -43,21 +42,20 @@
     <header class="d-flex">
         <div class="mb-1 mt-1 ms-1 me-1 targetListHeader subHeader1">
             <div class="h-100 w-100 fs-2 d-flex flex-nowrap justify-content-center align-items-center">
-                <a href="/index.html.php" class="position-absolute start-0">
+                <a href="/" class="position-absolute back btn start-0 mb-0 ms-3 p-0 pe-2 pt-1 pb-1"
+                data-bs-toggle="tooltip"
+                data-bs-placement="bottom"
+                data-bs-title="Back to tests"
+                data-bs-custom-class="custom-tooltip">
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M15 18L9 12L15 6" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
+                    <span class="d-none">Back</span>
                 </a>
 
                 <h3 class="headerTitle text-center m-0">Targets</h3>
             </div>
         </div>
-
-        <!-- <div class="mb-1 mt-1 ms-0 me-1 targetListHeader subHeader2">
-            <div class="h-100 w-100 fs-2 d-flex flex-nowrap justify-content-center align-items-center">
-                <h3 class="headerTitle text-center m-0">Vulnerabilities</h3>
-            </div>
-        </div> -->
 
         <div class="mb-1 mt-1 ms-0 me-1 targetListHeader flex-grow-1">
             <div id="vulnerabilityDetails" class="h-100 fs-2 d-flex flex-nowrap justify-content-start align-items-center">
@@ -73,15 +71,10 @@
         <div id="targetSidebar" class="h-100 ms-1 me-0 overflow-auto">
 
 
-            <div id="targetAccordion" class="target-list h-100 w-100">
+            <div id="targetAccordion" class="accordion accordion-flush p-0 m-0 target-list h-100 w-100">
 
             </div>
         </div>
-
-
-        <!-- <div id="vulnerabilitySidebar" class="vulnerability-list">
-
-        </div> -->
 
         <div id="vulnerabilityDetails" class="h-100 w-100 ms-1 me-1 overflow-auto">
             <div class="vulnerability-details h-100 w-100">
@@ -91,8 +84,5 @@
 
     </div>
 
-    <!-- <div class="target-list"></div> -->
-    <!-- <div class="vulnerability-list"></div> -->
-    <!-- <div class="vulnerability-details"></div> -->
 </body>
 </html>
