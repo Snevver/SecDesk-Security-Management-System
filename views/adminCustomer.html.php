d98920f94bba40759f01dba93688a89607820138
Snevver <svenhoeksema@hotmail.com>
2025-06-24 11:13:07 +0200

    quick alter

--- views/adminCustomer.html.php
+++ views/adminCustomer.html.php
@@ -251,7 +251,6 @@
                                         <th>Status</th>
                                         <th>Pentester</th>
                                         <th>Test Date</th>
-                                        <th>Actions</th>
                                     </tr>
                                 </thead>
                                 <tbody id="tests-table-body">
@@ -276,7 +275,6 @@
                                         <th>Test</th>
                                         <th>Name</th>
                                         <th>Description</th>
-                                        <th>Actions</th>
                                     </tr>
                                 </thead>                                
                                 <tbody id="targets-table-body">
