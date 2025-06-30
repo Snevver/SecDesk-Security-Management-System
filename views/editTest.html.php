5d743f28faca8317eedf7e9ccb28873343f886ad
Penguin <son@vdburg.site>
2025-06-30 10:52:04 +0200

    vuln form added

--- views/editTest.html.php
+++ views/editTest.html.php
@@ -60,7 +60,9 @@
 
             <!-- Target form -->
             <form class="d-none flex-column gap-3" id="target-form">
-                <h1>Target Form</h1>                <div>
+                <h1>Target Form</h1> 
+
+                <div>
                     <label for="target-title-input">Title:</label>
                     <input type="text" id="target-title-input" name="target-title" value="Loading..." required>
                 </div>
@@ -70,6 +72,100 @@
                 </div>
                 <div>
                     <button type="submit" id="target-submit">Save Changes</button>
+                </div>
+            </form>
+
+            <!-- Vulnerability Form -->
+            <form class="d-none flex-column gap-3" id="vulnerability-form">
+                <h1>Vulnerability Form</h1>           
+
+                <div>
+                    <label for="vulnerability-title-input">Title:</label>
+                    <input type="text" id="vulnerability-title-input" name="vulnerability-title" value="Loading..." required>
+                </div>
+
+                <div>
+                    <label for="vulnerability-identifier-input">Identifier:</label>
+                    <input type="text" id="vulnerability-identifier-input" name="vulnerability-identifier" value="Loading..." required>
+                </div>
+
+                <div>
+                    <label for="vulnerability-risk-statement-input">Risk statement:</label>
+                    <input type="text" id="vulnerability-risk-statement-input" name="vulnerability-risk-statement" value="Loading..." required>
+                </div>
+
+                <div>
+                    <label for="vulnerability-affected-component-input">Affected Component:</label>
+                    <input type="text" id="vulnerability-affected-component-input" name="vulnerability-affected-component" value="Loading..." required>
+                </div>
+
+                <div>
+                    <label for="vulnerability-residual-risk-input">Residual Risk:</label>
+                    <input type="text" id="vulnerability-residual-risk-input" name="vulnerability-residual-risk" value="Loading..." required>
+                </div>
+
+                <div>
+                    <label for="vulnerability-classification-input">classification:</label>
+                    <input type="text" id="vulnerability-classification-input" name="vulnerability-classification" value="Loading..." required>
+                </div>
+
+                <div>
+                    <label for="vulnerability-identified-controls-input">identified Controls:</label>
+                    <input type="text" id="vulnerability-identified-controls-input" name="vulnerability-identified-controls" value="Loading..." required>
+                </div>
+
+                <div>
+                    <label for="vulnerability-cvss-score-input">CVSS Score:</label>
+                    <input type="number" id="vulnerability-cvss-score-input" name="vulnerability-cvss-score" value="Loading..." required>
+                </div>
+
+                <div>
+                    <label for="vulnerability-likelihood-input">Likelihood:</label>
+                    <input type="text" id="vulnerability-likelihood-input" name="vulnerability-likelihood" value="Loading..." required>
+                </div>
+
+                <div>
+                    <label for="vulnerability-cvssv3-code-input">CVSSV3 Code:</label>
+                    <input type="text" id="vulnerability-cvssv3-code-input" name="vulnerability-cvssv3-code" value="Loading..." required>
+                </div>
+
+                <div>
+                    <label for="vulnerability-location-input">Location:</label>
+                    <input type="text" id="vulnerability-location-input" name="vulnerability-location" value="Loading..." required>
+                </div>
+
+                <div>
+                    <label for="vulnerability-description-input">Description:</label>
+                    <input type="text" id="vulnerability-description-input" name="vulnerability-description" value="Loading..." required>
+                </div>
+
+                <div>
+                    <label for="vulnerability-reproduction-steps-input">Reproduction Steps:</label>
+                    <input type="text" id="vulnerability-reproduction-steps-input" name="vulnerability-reproduction-steps" value="Loading..." required>
+                </div>
+
+                <div>
+                    <label for="vulnerability-impact-input">Impact:</label>
+                    <input type="text" id="vulnerability-impact-input" name="vulnerability-impact" value="Loading..." required>
+                </div>
+
+                <div>
+                    <label for="vulnerability-remediation-difficulty-input">Remediation Difficulty:</label>
+                    <input type="text" id="vulnerability-remediation-difficulty-input" name="vulnerability-remediation-difficulty" value="Loading..." required>
+                </div>
+
+                <div>
+                    <label for="vulnerability-response-input">Response:</label>
+                    <input type="text" id="vulnerability-response-input" name="vulnerability-response" value="Loading..." required>
+                </div>
+
+                <div>
+                    <label for="vulnerability-solved-input">Solved?</label>
+                    <input type="radio" id="vulnerability-solved-input" name="vulnerability-solved" value="Loading..." required>
+                </div>
+
+                <div>
+                    <button type="submit" id="vulnerability-submit">Save Changes</button>
                 </div>
             </form>
         </div>
